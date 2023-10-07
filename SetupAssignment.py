b84a8577d7f9338091d21a63c6e5358a6848349b
achutha.chithimalli@ndsu.edu <Zeb170cr@ndsucs>
2023-10-07 18:42:20 -0500

    databricks assignment

--- SetupAssignment.py
+++ SetupAssignment.py
@@ -2,6 +2,9 @@
 # MAGIC %md
 # MAGIC # Databricks Setup Assignment
 # MAGIC This notebook is the basis for the execution portion of the Databricks setup assignment.  This will involve connecting to your storage account to execute a couple of pre-written basic commands.  Then you will a simple column add of your own and save the results.
+import dbutils
+from Tools.scripts.dutree import display
+from pyspark.shell import spark
 
 # COMMAND ----------
 
@@ -18,26 +21,26 @@ spark.conf.set(
 uri = "abfss://assign1@achuthastorage.dfs.core.windows.net/"
 
 # Reading the data file from the storage account.  
-sp_df = spark.read.csv(uri+'SandP500Daily.csv', header=True)
+datafile = spark.read.csv(uri+'SandP500Daily.csv', header=True)
  
-display(sp_df)
+display(datafile)
 
 # Creating the new column with the range for the day.
-sp_range_df = sp_df.withColumn('Range', sp_df.High - sp_df.Low)
+range_column_datafile = datafile.withColumn('Range', datafile.High - datafile.Low)
 
-display(sp_range_df)
+display(range_column_datafile)
 
 # Saving this range file to a single CSV. Using coalesce to output it to a single file.
-sp_range_df.coalesce(1).write.option('header',True).mode('overwrite').csv(uri+"output/Range")
+range_column_datafile.coalesce(1).write.option('header',True).mode('overwrite').csv(uri+"output/Range")
 
 # Using the range from the previous cells to find the percent change for each day.
-sp_range_df_percent_change = sp_range_df.withColumn('Percent Change', (sp_range_df.Range/sp_range_df.Open) * 100)
+range_datafile_percent_change = range_column_datafile.withColumn('Percent Change', (range_column_datafile.Range/range_column_datafile.Open) * 100)
 
 # Sorting the dataset descending based on the percent change (High to Low).
-sp_range_df_sorted = sp_range_df_percent_change.orderBy("Percent Change", ascending=False)
+range_datafile_sorted = range_datafile_percent_change.orderBy("Percent Change", ascending=False)
 
-display(sp_range_df_sorted)
+display(range_datafile_sorted)
 
 # Saving the file to a single CSV file to storage account in the location output/PercentChange.
-sp_range_df_sorted.coalesce(1).write.option('header',True).mode('overwrite').csv(uri+"output/PercentChange")
+range_datafile_sorted.coalesce(1).write.option('header',True).mode('overwrite').csv(uri+"output/PercentChange")
 
